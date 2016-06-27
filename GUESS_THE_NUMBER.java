c932b41ed0de2f531e83b91d22870cf1d84aa3f4
plipinski123 <Guest@stk-cs01-ws13.westlothian.sch.uk>
2016-06-27 10:33:12 +0100

    version 1

---
+++ GUESS_THE_NUMBER.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class GUESS_THE_NUMBER here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class GUESS_THE_NUMBER
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class GUESS_THE_NUMBER
+     */
+    public GUESS_THE_NUMBER()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
